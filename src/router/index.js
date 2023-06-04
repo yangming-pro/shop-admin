9d763f36f42b95929a618a696367f7dab2d9ca49
yangming <yangming_pro@163.com>
2023-06-04 19:10:41 +0800

    [dev] 引入windicss、vue-router 、 vite.config增加src别名

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import {
+    createRouter,
+    createWebHashHistory,
+} from "vue-router";
+
+import Index from "~/pages/index.vue";
+import About from "~/pages/about.vue";
+const routes = [
+    {
+        path: "/",
+        component: Index,
+    },
+    {
+        path: "/about",
+        component: About,
+    },
+];
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+});
+
+export default router;
